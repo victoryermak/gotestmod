fd5b2a4e1badaa6c7072ec127ddbe495b4908ee7
victoryermak <victor.yermak@gmail.com>
2020-02-29 19:28:28 +0200

    major ver

--- gotestmod.go
+++ gotestmod.go
@@ -3,6 +3,10 @@ package gotestmod
 import "fmt"
 
 // Hi returns a friendly greeting
-func Hi(name string) string {
+func Hi(name string, residence string) string {
+  if residence =="UA" {
    return fmt.Sprintf("Hi, %s!!!", name)
+  } else{
+    return fmt.Sprintf("Fuck you, %s!!!", name)
+  }
 }
